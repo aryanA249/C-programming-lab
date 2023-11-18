c034c9d3f7de552135a647a514037cb662fb667c
Aryan <aryansmasher.9596@gmail.com>
2023-11-18 18:27:44 +0530

    done

---
+++ armstrng.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    int num, originalNum, remainder, n = 0, result = 0;
+
+    // Read the number from the user
+    printf("Enter a positive integer: ");
+    scanf("%d", &num);
+
+    originalNum = num;
+
+    // Count the number of digits
+    while (originalNum != 0) {
+        originalNum /= 10;
+        ++n;
+    }
+
+    originalNum = num;
+
+    // Calculate the Armstrong number
+    while (originalNum != 0) {
+        remainder = originalNum % 10;
+        result += pow(remainder, n);
+        originalNum /= 10;
+    }
+
+    // Check if the number is an Armstrong number
+    if (result == num) {
+        printf("%d is an Armstrong number.\n", num);
+    } else {
+        printf("%d is not an Armstrong number.\n", num);
+    }
+
+    return 0;
+}
