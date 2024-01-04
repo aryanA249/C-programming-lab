e7bf64c5007f6cfec98503517bbfd08e5d1c00eb
Aryan <aryansmasher.9596@gmail.com>
2024-01-04 14:03:18 +0530

    7.1

---
+++ 2-darray.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main() {
+    int rows, cols;
+
+    // Input the number of rows and columns
+    printf("Enter the number of rows: ");
+    scanf("%d", &rows);
+    printf("Enter the number of columns: ");
+    scanf("%d", &cols);
+
+    // Declare a 2D array of size rows x cols
+    int arr[rows][cols];
+
+    // Input the elements of the 2D array
+    printf("Enter %d x %d elements:\n", rows, cols);
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            scanf("%d", &arr[i][j]);
+        }
+    }
+
+    // Display the elements of the 2D array
+    printf("Elements of the 2D array:\n");
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            printf("%d\t", arr[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
